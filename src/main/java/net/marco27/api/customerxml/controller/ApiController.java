79c1cf663bc98a74562727f09a5122e378fc0efa
marcoguastalli <marcode1q2w3e>
2019-11-24 11:24:34 +0100

    Customer Xml MongoDB customerName

--- src/main/java/net/marco27/api/customerxml/controller/ApiController.java
+++ src/main/java/net/marco27/api/customerxml/controller/ApiController.java
@@ -5,34 +5,29 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import net.marco27.api.customerxml.model.Customer;
 import net.marco27.api.customerxml.model.Customers;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import net.marco27.api.customerxml.repository.CustomerRepository;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-	
+
     @Autowired
     private CustomerRepository customerRepository;
 
-    @PostMapping(value="/customer", produces=MediaType.APPLICATION_XML_VALUE)
-	public Customer postCustomer(@RequestBody Customer customer) {
-		return customerRepository.save(customer);
-	}
-    
-    @GetMapping(value="/customer", produces=MediaType.APPLICATION_XML_VALUE)
+    @PostMapping(value = "/customer", produces = MediaType.APPLICATION_XML_VALUE)
+    public Customer postCustomer(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @GetMapping(value = "/customer", produces = MediaType.APPLICATION_XML_VALUE)
     public Customers getAllCustomers() {
         List<Customer> customer = (List<Customer>) customerRepository.findAll();
         Customers customers = new Customers();
@@ -40,31 +35,31 @@ public class ApiController {
 
         return customers;
     }
-    
-    @GetMapping(value="/customer/{id}", produces=MediaType.APPLICATION_XML_VALUE)
+
+    @GetMapping(value = "/customer/{id}", produces = MediaType.APPLICATION_XML_VALUE)
     public Customer getCustomerById(@PathVariable String id) {
-    	Optional<Customer> optCustomer = customerRepository.findById(id);
-    	
-    	if(optCustomer.isPresent()) {
-    		return customerRepository.findById(id).get();
-    	}
-    	
-    	throw new RuntimeException("Not Found a customer with id = " + id);
+        Optional<Customer> optCustomer = customerRepository.findById(id);
+
+        if (optCustomer.isPresent()) {
+            return customerRepository.findById(id).get();
+        }
+
+        throw new RuntimeException("Not Found a customer with id = " + id);
     }
-    
-    @PutMapping(value="/customer/{id}", produces=MediaType.APPLICATION_XML_VALUE)
+
+    @PutMapping(value = "/customer/{id}", produces = MediaType.APPLICATION_XML_VALUE)
     public Customer putCustomer(@PathVariable String id,
             @Valid @RequestBody Customer customerUpdated) {
-    	
-    	return customerRepository.findById(id)
-    			.map(customer -> {
+
+        return customerRepository.findById(id)
+                .map(customer -> {
                     customer.setFirstname(customerUpdated.getFirstname());
                     customer.setLastname(customerUpdated.getLastname());
                     customer.setAge(customerUpdated.getAge());
-                    return  customerRepository.save(customer);
+                    return customerRepository.save(customer);
                 }).orElseThrow(() -> new RuntimeException("Customer not found with id = " + id));
     }
-    
+
     @DeleteMapping("/customer/{id}")
     public String deleteCustomer(@PathVariable String id) {
         return customerRepository.findById(id)
@@ -72,5 +67,17 @@ public class ApiController {
                     customerRepository.delete(customer);
                     return "Delete Successfully!";
                 }).orElseThrow(() -> new RuntimeException("Customer not found with id = " + id));
+    }
+
+    @GetMapping(value = "/customerName/{id}")
+    public ResponseEntity<String> getCustomer(@PathVariable String id) {
+        Optional<Customer> optCustomer = customerRepository.findById(id);
+
+        if (optCustomer.isPresent()) {
+            return ResponseEntity.ok(optCustomer.get().getFirstname());
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
 }
